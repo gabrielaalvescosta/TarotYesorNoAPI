44227c67a8f4b4144832c277701e3904c624cfd7
Gabriela Costa <gabrielasilvaalvescosta@gmail.com>
2022-01-15 02:27:57 -0300

    Alteração nome API

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 // Importação do express
 const customExpress = require('./config/custom-express')
-const autores = require('./controllers/tarotController')
+const tarot = require('./controllers/tarotController')
 
 // Config de app e porta
 const app = customExpress()
 const PORT = process.env.PORT || 3000
 
 // Importação das funções controllers
-autores(app)
+tarot(app)
 
 //Listener da porta
 app.listen(PORT, () => {
